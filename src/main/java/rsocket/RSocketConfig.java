f949a89ae91b7f56fdf6b3053a0df5a961117495
Tom1989 <thomas.walciszewski@gmail.com>
2024-01-24 16:36:54 +0100

    Sending messages bidirectionally with channel communication model

--- src/main/java/rsocket/RSocketConfig.java
+++ src/main/java/rsocket/RSocketConfig.java
@@ -1,28 +1,40 @@
 package rsocket;
 
+import java.math.BigDecimal;
+import java.time.Duration;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.rsocket.RSocketRequester;
 
 import lombok.extern.slf4j.Slf4j;
-import java.time.Instant;
+import reactor.core.publisher.Flux;
 
 @Configuration
 @Slf4j
-public class RSocketConfig {
+public class RsocketConfig {
 
     @Bean
-    public ApplicationRunner sender(RSocketRequester.Builder requesterBuilder) {
+    public ApplicationRunner runner(RSocketRequester.Builder requesterBuilder) {
         return args -> {
             RSocketRequester tcp = requesterBuilder.tcp("localhost", 7000);
-            tcp
-                .route("alert")
-                .data(new Alert(Alert.Level.RED, "TOM", Instant.now()))
-                .send()
-                .subscribe();
+            Flux<GratuityIn> gratuityInFlux = Flux.fromArray(new GratuityIn[] {
+                new GratuityIn(new BigDecimal(35.50), 18),
+                new GratuityIn(new BigDecimal(10.00), 15),
+                new GratuityIn(new BigDecimal(23.25), 20),
+                new GratuityIn(new BigDecimal(52.75), 18),
+                new GratuityIn(new BigDecimal(80.00), 15)
+            })
+            .delayElements(Duration.ofSeconds(1));
 
-            log.info("Alert sent");
+            tcp
+                .route("gratuity")
+                .data(gratuityInFlux)
+                .retrieveFlux(GratuityOut.class)
+                .subscribe(out -> log.info(out.getPercent() + "% gratuity on "
+                    + out.getBillTotal() + " is "
+                    + out.getGratuity()));
         };
     }
 }
